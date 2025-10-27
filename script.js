688e4bf581b71b15b8813558b3f6a2bfbdb4f722
gustavoluiz15 <costa.gustavo15@escola.pr.gov.br>
2025-10-27 15:30:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ buttons.forEach(button => {
   button.addEventListener("click", () => {
     buttons.forEach(btn => btn.classList.remove("active"));
     tabs.forEach(tab => tab.classList.remove("active"));
-
     button.classList.add("active");
     document.getElementById(button.dataset.tab).classList.add("active");
   });
