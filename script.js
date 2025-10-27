d1051d545ddc03229278653a86e22858084a8906
gustavoluiz15 <costa.gustavo15@escola.pr.gov.br>
2025-10-27 15:24:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-// Alternância das abas
-const buttons = document.querySelectorAll('.tab-button');
-const contents = document.querySelectorAll('.tab-content');
+const buttons = document.querySelectorAll(".tab-button");
+const tabs = document.querySelectorAll(".tab-content");
 
 buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    buttons.forEach(btn => btn.classList.remove('active'));
-    contents.forEach(tab => tab.classList.remove('active'));
+  button.addEventListener("click", () => {
+    buttons.forEach(btn => btn.classList.remove("active"));
+    tabs.forEach(tab => tab.classList.remove("active"));
 
-    button.classList.add('active');
-    document.getElementById(button.dataset.tab).classList.add('active');
+    button.classList.add("active");
+    document.getElementById(button.dataset.tab).classList.add("active");
   });
 });
